13849e0ce158ce6abce0a30958b30dd869ea1ec2
Shamanoff <shamanoff@gmail.com>
2017-11-21 16:36:00 +0200

    work

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { ConferenceSpeakerModule } from './speaker/speaker.module';
+import { ConferenceSessionModule } from './session/session.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        ConferenceSpeakerModule,
+        ConferenceSessionModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
